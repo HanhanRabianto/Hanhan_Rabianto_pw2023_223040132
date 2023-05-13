a426a0c9e9651d27feea87c829793b7fccc25185
Hanhan32 <125179054+HanhanRabianto@users.noreply.github.com>
2023-05-13 23:10:12 +0700

    Add files via upload

---
+++ tubes/db.php
@@ -0,0 +1,8 @@
+<?php  
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname   = 'db_landgaming';
+
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+?>
